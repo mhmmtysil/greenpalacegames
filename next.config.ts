180ac5ed920cc30a8341ffc3f09d9a6595f4411b
mhmmtysil <muhammetyesil@outlook.com.tr>
2025-11-07 21:57:23 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/greenpalacegames' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/greenpalacegames/' : '',
 };
 
 export default nextConfig;
